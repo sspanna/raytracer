213dcf1d1768d9c08235a788468911e58cb08438
sspanna <tianfl@gmail.com>
2026-07-09 15:49:12 -0700

    clamp color

--- color.h
+++ color.h
@@ -1,14 +1,17 @@
 #pragma once
+#include "interval.h"
 #include "vec3.h"
 #include <stdio.h>
 
 typedef vec3 color;
 
+static const interval intensity = (interval){0.000, 0.999};
+
 static inline void write_color(FILE *out, const color pixel_color)
 {
-  int rbyte = (int)(255.999 * pixel_color.x);
-  int gbyte = (int)(255.999 * pixel_color.y);
-  int bbyte = (int)(255.999 * pixel_color.z);
+  int rbyte = (int)(256 * interval_clamp(intensity, pixel_color.x));
+  int gbyte = (int)(256 * interval_clamp(intensity, pixel_color.y));
+  int bbyte = (int)(256 * interval_clamp(intensity, pixel_color.z));
 
   fprintf(out, "%d %d %d\n", rbyte, gbyte, bbyte);
 }
